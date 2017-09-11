c62b5a9496ff4dd449af3de8401cebece7b5a201
Ovidiu Visan <extern.visan_ovidiu1@allianz.de>
2017-09-11 13:37:20 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ansible-mac
-Ansible role for Mac config
+# Ansible role for MAC configuration
+
+## Instructions:
+run `./mac_config.sh`
---
+++ mac_conf.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Install homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
+
+echo "Install ansible"
+if ! brew install ansible ; then
+	echo "Upgrade instead"
+    brew upgrade ansible
+fi
+
+echo "Run Ansible role"
+ansible-playbook -t mac_conf mac_conf.yml
